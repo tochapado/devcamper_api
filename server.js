b86f8c2e4eabd8e9a691e10956c5246e8705a250
tochapado <tochapado710@gmail.com>
2024-11-05 14:22:04 -0300

    Connects to MongoDB and creates Bootcamp Schema

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+
+// Load env vars
+dotenv.config({ path: './config/config.env', });
+
+// Connect to database
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps.js');
 
-// Load env vars
-dotenv.config({ path: './config/config.env', });
 
 const app = express();
 
@@ -14,4 +19,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 6969;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', function(err, promise) {
+  console.log('Error: ' + err.message);
+
+  // CLose server & exit process
+  server.close(function() {
+    return process.exit(1);
+  });
+});
