9242bd202dd637e0c04c2e537822df6337b2f0b3
tochapado <tochapado710@gmail.com>
2024-11-07 08:09:04 -0300

    Adds pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,7 +12,7 @@ const getBootcamps = asyncHandler(async function(req, res, next) {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // Loop over removeFields and delete from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -40,14 +40,41 @@ const getBootcamps = asyncHandler(async function(req, res, next) {
     query = query.sort('-createdAt');
   };
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if(endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  };
+
+  if(startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: limit
+    };
+  };
 
   res
     .status(200)
     .json({
       success: true,
       count: bootcamps.length,
+      pagination: pagination,
       data: bootcamps,
     });
 });
