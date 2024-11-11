9a48d1891d60035b2b9d710d260654e7061a81e4
tochapado <tochapado710@gmail.com>
2024-11-11 07:36:18 -0300

    Adds Course model

--- seeder.js
+++ seeder.js
@@ -9,17 +9,21 @@ dotenv.config({
 
 // Load models
 const Bootcamp = require('./models/Bootcamp.js');
+const Course = require('./models/Course.js');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(__dirname + '/_data/bootcamps.json', 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(__dirname + '/_data/courses.json', 'utf-8'));
 
 // Import into DB
 const importData = async function() {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+
     console.log('Data imported..');
     process.exit();
   } catch(err) {
@@ -31,6 +35,8 @@ const importData = async function() {
 const deleteData = async function() {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
+
     console.log('Data destroyed..');
     process.exit();
   } catch(err) {
